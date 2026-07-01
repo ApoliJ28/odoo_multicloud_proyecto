980bc27bf6311898c5b775039eadf1caa3e024f9
ApoliJ28 <apolinaresvictor@gmail.com>
2026-07-01 13:42:48 -0400

    feat: se agrega la variable de entorno session_token para podernos conectarnos aws con aws academy.

--- terraform/aws/providers.tf
+++ terraform/aws/providers.tf
@@ -19,6 +19,8 @@ provider "aws" {
   region = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
+  token      = var.session_token
+  
   default_tags {
     tags = {
       Project     = "Odoo-Multicloud"
--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -19,3 +19,8 @@ variable "secret_key" {
     description = "AWS secret key"
     type        = string
 }
+
+variable "session_token" {
+    description = "AWS session token"
+    type        = string
+}
