638a835e86c80a5573574b081693a8cd50126bc8
ApoliJ28 <apolinaresvictor@gmail.com>
2026-07-02 00:25:26 -0400

    Se crea tablas de enrutamientos para distribuir todo el trafico publico.

--- terraform/aws/modules/vpc/main.tf
+++ terraform/aws/modules/vpc/main.tf
@@ -30,3 +30,22 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
   tags = { Name = "odoo-igw" }
 }
+
+# Se crea la tabla de rutas apuntando al Internet Gateway
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = { Name = "odoo-public-rt" }
+}
+
+# Se asocia esta tabla de rutas exclusivamente a las subredes públicas
+resource "aws_route_table_association" "public" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
